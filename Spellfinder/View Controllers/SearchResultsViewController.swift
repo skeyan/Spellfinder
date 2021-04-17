8717c525c2aa861bd26a6cd07dd91c7c9cde9b24
skeyan <eyan0749@gmail.com>
2021-04-17 16:14:21 -0400

    Set up separate table views for search result screen

--- Spellfinder/View Controllers/SearchResultsViewController.swift
+++ Spellfinder/View Controllers/SearchResultsViewController.swift
@@ -8,11 +8,26 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+    @IBOutlet var staticTableView: UITableView!
+    @IBOutlet var tableView: UITableView!
+    
+    struct TableView {
+      struct CellIdentifiers {
+        static let searchResultCell = "SearchResultCell"
+        static let nothingFoundCell = "NothingFoundCell"
+        static let loadingCell = "LoadingCell"
+      }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Register nibs
+        let cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
+        // TO-DO: Replace with Search and Filter cells
+        staticTableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
     }
     
 
@@ -26,4 +41,49 @@ class SearchResultsViewController: UIViewController {
     }
     */
 
+}
+
+extension SearchResultsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if tableView === staticTableView {
+            return 2
+        } else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView === staticTableView {
+            return 1
+        } else {
+            return 10
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView === staticTableView {
+            let cellIdentifier = TableView.CellIdentifiers.searchResultCell
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: cellIdentifier,
+                for: indexPath)
+            cell.backgroundColor = UIColor.yellow
+            return cell
+        } else {
+            let cellIdentifier = TableView.CellIdentifiers.searchResultCell
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: cellIdentifier,
+                for: indexPath)
+            return cell
+        }
+    }
+    
+    // UI improvement - change table view cell height to accomodate for XIB
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if tableView === staticTableView {
+            return 60
+        } else {
+            return 88
+        }
+    }
+  
 }
