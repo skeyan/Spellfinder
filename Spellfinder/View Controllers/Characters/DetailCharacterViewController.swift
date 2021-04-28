83895f80e90a70303756a4c35938bbffa58bd62b
skeyan <eyan0749@gmail.com>
2021-04-28 17:24:03 -0400

    Make some ui improvements in character detail and implemented fetch delegates as well

--- Spellfinder/View Controllers/Characters/DetailCharacterViewController.swift
+++ Spellfinder/View Controllers/Characters/DetailCharacterViewController.swift
@@ -124,9 +124,82 @@ class DetailCharacterViewController: UIViewController, NSFetchedResultsControlle
             totalNumberOfSpellsValueLabel.text = "0"
         }
     }
+    
+    // MARK: - NSFetchedResultsController Delegate
+    func controllerWillChangeContent(
+      _ controller: NSFetchedResultsController<NSFetchRequestResult>
+    ) {
+      print("*** controllerWillChangeContent")
+      tableView.beginUpdates()
+    }
+
+    func controller(
+      _ controller: NSFetchedResultsController<NSFetchRequestResult>,
+      didChange anObject: Any,
+      at indexPath: IndexPath?,
+      for type: NSFetchedResultsChangeType,
+      newIndexPath: IndexPath?
+    ) {
+      switch type {
+      case .insert:
+        print("*** NSFetchedResultsChangeInsert (object)")
+        tableView.insertRows(at: [newIndexPath!], with: .fade)
+
+      case .delete:
+        print("*** NSFetchedResultsChangeDelete (object)")
+        tableView.deleteRows(at: [indexPath!], with: .fade)
+
+      case .update:
+        print("*** NSFetchedResultsChangeUpdate (object)")
+        if let cell = tableView.cellForRow(
+          at: indexPath!) as? FavoritesCell {
+          let characterSpell = controller.object(
+            at: indexPath!) as! Spell
+          cell.configure(for: characterSpell)
+        }
+
+      case .move:
+        print("*** NSFetchedResultsChangeMove (object)")
+        tableView.deleteRows(at: [indexPath!], with: .fade)
+        tableView.insertRows(at: [newIndexPath!], with: .fade)
+        
+      @unknown default:
+        print("*** NSFetchedResults unknown type")
+      }
+    }
+
+    func controller(
+      _ controller: NSFetchedResultsController<NSFetchRequestResult>,
+      didChange sectionInfo: NSFetchedResultsSectionInfo,
+      atSectionIndex sectionIndex: Int,
+      for type: NSFetchedResultsChangeType
+    ) {
+      switch type {
+      case .insert:
+        print("*** NSFetchedResultsChangeInsert (section)")
+        tableView.insertSections(
+          IndexSet(integer: sectionIndex), with: .fade)
+      case .delete:
+        print("*** NSFetchedResultsChangeDelete (section)")
+        tableView.deleteSections(
+          IndexSet(integer: sectionIndex), with: .fade)
+      case .update:
+        print("*** NSFetchedResultsChangeUpdate (section)")
+      case .move:
+        print("*** NSFetchedResultsChangeMove (section)")
+      @unknown default:
+        print("*** NSFetchedResults unknown type")
+      }
+    }
+      
+    func controllerDidChangeContent(
+      _ controller: NSFetchedResultsController<NSFetchRequestResult>
+    ) {
+      print("*** controllerDidChangeContent")
+      tableView.endUpdates()
+    }
 }
 
-// TO-DO: Table View load the character's spells with nsfetchedresultscontroller
 // MARK: - Table View Delegates
 extension DetailCharacterViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -142,10 +215,19 @@ extension DetailCharacterViewController: UITableViewDelegate, UITableViewDataSou
         cell.delegate = self
         
         // Get the data to display in the cell from Core Data
-        let favoritedSpell = fetchedResultsController.object(at: indexPath)
-        cell.configure(for: favoritedSpell)
-        cell.data = favoritedSpell
+        let characterSpell = fetchedResultsController.object(at: indexPath)
+        cell.configure(for: characterSpell)
+        cell.data = characterSpell
         
         return cell
+    }
+    
+    // UI improvement - deselection animation
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Perform segue to Spell Detail
+        // let cell = tableView.cellForRow(at: indexPath) as? FavoritesCell
+        // performSegue(withIdentifier: "ShowFavoritesSpellDetail", sender: cell)
     }
 }
