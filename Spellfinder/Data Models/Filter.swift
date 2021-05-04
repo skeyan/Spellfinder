749257c4a8bd368994d5974592e69281d9b0d5d6
skeyan <eyan0749@gmail.com>
2021-05-03 21:55:57 -0400

    Added 'create filter' and 'filter to string' functions

--- Spellfinder/Data Models/Filter.swift
+++ Spellfinder/Data Models/Filter.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class Filter {
+    
+    // MARK: - Instance Variables
+    var levelFilter: Set<Double> = []
+    var classFilter: Set<String> = []
+    var componentsFilter: Set<String> = []
+    var schoolFilter: Set<String> = []
+    var concentrationFilter: Set<String> = []
+    
+    // Format information for debugging
+    var description: String {
+        return "Filter: \nLevel: \(levelFilter)\n Class: \(classFilter)\n" +
+               "Components: \(componentsFilter)\n School: \(schoolFilter)\n Concentration: \(concentrationFilter)"
+    }
+}
